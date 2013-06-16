bc19234b6382f263f91439cd130868ed3c42ff9d
naironics <naironics@gmail.com>
2013-06-16 16:22:12 -0700

    Java program to demonstrate the usage of Spring Inner Beans , Alias

---
+++ SpringInnerBean/src/com/naironics/innerbean/DrawingApp.java
@@ -0,0 +1,14 @@
+package com.naironics.innerbean;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class DrawingApp {
+	public static void main(String[] args){
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+		Triangle triangle = (Triangle)ctx.getBean("triangle");
+		triangle.draw();
+		
+	}
+
+}
